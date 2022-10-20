813fb856c01d011da8fa35390f1597eadd1cf388
sley avila <zleysithafc@gmail.com>
2022-10-19 20:12:39 -0500

    Se arreglo el nombre de las actividades

--- app/src/main/java/com/miempresa/findapp03/ListaBancosActivity.java
+++ app/src/main/java/com/miempresa/findapp03/ListaBancosActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class activity_lista_bancos extends AppCompatActivity {
+public class ListaBancosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/miempresa/findapp03/SplashActivity.java
+++ app/src/main/java/com/miempresa/findapp03/SplashActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class activity_splash extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +17,7 @@ public class activity_splash extends AppCompatActivity {
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
-                Intent i = new Intent(activity_splash.this, MainActivity.class);
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(i);
             }
         }, 5000);
