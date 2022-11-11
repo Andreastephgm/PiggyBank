12aa59c5ebef9896b63650ac6d9c1e1f555a4958
Fore4066 <jairfernandoforero@gmail.com>
2022-11-10 20:38:15 -0500

    Intents y Menus

--- app/src/main/java/com/miempresa/findapp03/AdmActivity.java
+++ app/src/main/java/com/miempresa/findapp03/AdmActivity.java
@@ -2,18 +2,15 @@ package com.miempresa.findapp03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.android.material.snackbar.Snackbar;
 
 public class AdmActivity extends AppCompatActivity {
 
     Button buttonCrearBanco;
+    Button ListaLB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +18,20 @@ public class AdmActivity extends AppCompatActivity {
         setContentView(R.layout.activity_adm);
 
         buttonCrearBanco = findViewById(R.id.buttonCrearBanco);
+        ListaLB= findViewById(R.id.buttonLBANCOS);
         buttonCrearBanco.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i  = new Intent(AdmActivity.this, AgregarBanco.class);
                 startActivity(i);
 
+            }
+        });
+        ListaLB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i  = new Intent(AdmActivity.this, ListarBancos.class);
+                startActivity(i);
             }
         });
     }
--- app/src/main/java/com/miempresa/findapp03/activity_profile.java
+++ app/src/main/java/com/miempresa/findapp03/activity_profile.java
@@ -1,9 +1,12 @@
 package com.miempresa.findapp03;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class activity_profile extends AppCompatActivity {
 
@@ -15,7 +18,30 @@ public class activity_profile extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_menu,menu);
+        getMenuInflater().inflate(R.menu.activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.itemSalir) {
+            Intent i = new Intent(activity_profile.this, MainActivity.class);
+            startActivity(i);
+
+        }
+        if (item.getItemId() == R.id.itemLista) {
+            Intent i = new Intent(activity_profile.this, ListarBancos.class);
+            startActivity(i);
+        }
+        if (item.getItemId() == R.id.Itemperfil) {
+            Intent i = new Intent(activity_profile.this, activity_profile.class);
+            startActivity(i);
+
+        }
+        if (item.getItemId() == R.id.Itemhome) {
+            Intent i = new Intent(activity_profile.this, activity_welcome.class);
+            startActivity(i);
+        }
         return true;
     }
 }
